841ffc1c71557cd9d3704216116b078ae0ad71b6
Anna Bolger <bolgera@bc.edu>
2021-02-22 15:37:23 -0500

    events

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -8,19 +8,19 @@ export default function Events(){
         <div container className = "padding20">
             <Divider/>
                 <h2>
-                    Inspring Firsts
+                    Miscellaneous Opportunities
                 </h2>
                 <Grid container>
                 <Grid item lg = {6}>
                 <Card className = 'event'>
-                    <a href = 'https://www.forbes.com/sites/nancywang/2020/08/23/first-woman-director-at-mit-cs-ai-lab-want-more-women-in-stem-inspire-them-early/#471af876367e'>
+                    <a href = 'ehi.png'>
                         <Grid container justify = "center"  style= {{ padding: 0}}>
                             <Grid item lg = {4} sm = {12}>
-                                <img src= "forbeswis.jpg" style= {{width:"100px", height: "75px", padding: 0}} alt = 'forbes women in stem'/>
+                                <img src= "ehi.png" style= {{width:"100px", height: "75px", padding: 0}} alt = 'heights for health'/>
                             </Grid>
                             <Grid item lg = {8} sm = {12} className = 'articled'>
                                 <p>
-                                First Woman Director At MIT CSAIL: ‘Want More Women In STEM? Inspire Them Early.
+                                Engineering Healthcare Innovations Presents Heights 4 Health
                                 </p>
                             </Grid>
                         </Grid>
@@ -30,14 +30,15 @@ export default function Events(){
 
                 <Grid item lg = {6}>
                 <Card className = 'event'>
-                    <a href = 'https://ourcommunitynow.com/news-local/dr-jeanette-epps-makes-history-as-first-black-woman-recruited-for-nasa-international-space-station-mission'>
+                    <a href = 'cic.pdf'>
                         <Grid container justify = "center"  style= {{ padding: 0}}>
                             <Grid item lg = {4} sm = {12}>
-                                <img src= "nasa.png" style= {{width:"100px", height: "75px", padding: 0}} alt = 'our community now'/>
+                                <img src= "cic.png" style= {{width:"150px", height: "75px", padding: 0}} alt = 'civic innovation corps'/>
                             </Grid>
                             <Grid item lg = {8} sm = {12} className = 'articled'>
                                 <p>
-                                Dr. Jeanette Epps Makes History as First Black Woman Recruited for NASA International Space Station Mission                                </p>
+                                Civic Innovation Corps
+                                </p>
                             </Grid>
                         </Grid>
                     </a>
@@ -45,19 +46,29 @@ export default function Events(){
                 </Grid>
                 <Grid item lg = {6}>
                 <Card className = 'event'>
-                    <a href = 'https://www.nobelprize.org/prizes/chemistry/2020/summary/'>
+                    <a href = 'na.pdf'>
                         <Grid container justify = "center"  style= {{ padding: 0}}>
-                            <Grid container item lg = {4} sm = {12}>
-                                <Grid item lg = {6}>
-                                    <img src= "nobelPrize1.jpg" style= {{width:"70px", height: "100px", padding: 0}} alt = 'our community now'/>
+                                <Grid item lg = {4} sm = {12}>
+                                    <img src= "gartner.png" style= {{width:"100px", height: "75px", padding: 0}} alt = 'our community now'/>
                                 </Grid>
-                                <Grid item lg = {6}>
-                                    <img src= "nobelPrize2.jpg" style= {{width:"70px", height: "100px", padding: 0}} alt = 'our community now'/>
-                                </Grid>
-                            </Grid>
                             <Grid item lg = {8} sm = {12} className = 'articled'>
                                 <p>
-                                The Nobel Prize in Chemistry 2020 was awarded jointly to Emmanuelle Charpentier and Jennifer A. Doudna "for the development of a method for genome editing."
+                                Grow With Gartner: Student Leadership Summit                                </p>
+                            </Grid>
+                        </Grid>
+                    </a>
+                </Card>    
+                </Grid>
+                <Grid item lg = {6}>
+                <Card className = 'event'>
+                    <a href = 'na.pdf'>
+                        <Grid container justify = "center"  style= {{ padding: 0}}>
+                                <Grid item lg = {4} sm = {12}>
+                                    <img src= "mc.png" style= {{width:"100px", height: "75px", padding: 0}} alt = 'our community now'/>
+                                </Grid>
+                            <Grid item lg = {8} sm = {12} className = 'articled'>
+                                <p>
+                                Make friends during these tough times! New platform called Magic Connects created by CS major at UPENN.
                                 </p>
                             </Grid>
                         </Grid>
@@ -67,19 +78,19 @@ export default function Events(){
             </Grid>
             <Divider/>
                 <h2>
-                    Equity in Stem: 
+                    
                 </h2>
                 <Grid container>
                 <Grid item lg = {6}>
                 <Card className = 'event'>
-                    <a href = 'https://cen.acs.org/content/cen/articles/98/i23/Chemists-ShutDownSTEM-to-reflect-on-diversity-in-science.html?utm_source=Twitter&utm_medium=Social&utm_campaign=CEN'>
+                    <a href = 'ey.pdf'>
                         <Grid container justify = "center"  style= {{ padding: 0}}>
                             <Grid item lg = {4} sm = {12}>
-                                <img src= "missing.jpeg" style= {{width:"75px", height: "75px", padding: 0}} alt = 'chemists shut down to reflect on diversity'/>
+                                <img src= "ey.png" style= {{width:"75px", height: "75px", padding: 0}} alt = 'chemists shut down to reflect on diversity'/>
                             </Grid>
                             <Grid item lg = {8} sm = {12} className = 'articled'>
                                 <p>
-                                    Chemists #ShutDownSTEM to reflect on diversity in science
+                                Expedition EY — Technology Track
                                 </p>
                             </Grid>
                         </Grid>
@@ -88,30 +99,14 @@ export default function Events(){
                 </Grid>
                 <Grid item lg = {6}>
                 <Card className = 'event'>
-                    <a href = 'https://www.insidehighered.com/blogs/gradhacker/invisible-minority-stem'>
+                    <a href = 'crest.pdf'>
                         <Grid container justify = "center"  style= {{ padding: 0}}>
                             <Grid item lg = {4} sm = {12}>
-                                <img src= "lgbt.png" style= {{width:"75px", height: "75px", padding: 0}} alt = 'lgbt in stem'/>
+                                <img src= "crest.jpeg" style= {{width:"100px", height: "75px", padding: 0}} alt = 'diverse education'/>
                             </Grid>
                             <Grid item lg = {8} sm = {12} className = 'articled'>
                                 <p>
-                                    The Invisible Minority in STEM
-                                </p>
-                            </Grid>
-                        </Grid>                        
-                    </a>
-                </Card>
-                </Grid>
-                <Grid item lg = {6}>
-                <Card className = 'event'>
-                    <a href = 'https://diverseeducation.com/article/180193/'>
-                        <Grid container justify = "center"  style= {{ padding: 0}}>
-                            <Grid item lg = {4} sm = {12}>
-                                <img src= "diverse.png" style= {{width:"125px", height: "75px", padding: 0}} alt = 'diverse education'/>
-                            </Grid>
-                            <Grid item lg = {8} sm = {12} className = 'articled'>
-                                <p>
-                                    Can the Racial and Economic Justice Movement Help Advance Equity in Higher Education?
+                                The Commercial Real Estate Success Training (CREST) Program
                                 </p>
                             </Grid>
                         </Grid>
@@ -120,37 +115,19 @@ export default function Events(){
                 </Grid>
                 <Grid item lg = {6}>
                 <Card className = 'event'>
-                    <a href = 'https://community.chronicle.com/news/2372-a-survival-guide-for-black-indigenous-and-other-women-of-color-in-academe?cid=VTEVPMSED1'>
+                    <a href = 'hiring.pdf'>
                         <Grid container justify = "center"  style= {{ padding: 0}}>
                             <Grid item lg = {4} sm = {12}>
-                                <img src= "diversity.jpg" style= {{width:"125px", height: "75px", padding: 0}} alt = 'A Survival Guide for Black, Indigenous, and Other Women of Color in Academe'/>
+                                <img src= "linx.png" style= {{width:"100px", height: "75px", padding: 0}} alt = 'A Survival Guide for Black, Indigenous, and Other Women of Color in Academe'/>
                             </Grid>
                             <Grid item lg = {8} sm = {12} className = 'articled'>
                                 <p>
-                                A Survival Guide for Black, Indigenous, and Other Women of Color in Academia                                </p>
+                                    Linx Summer Camp Instructor
+                                </p>          
                             </Grid>
                         </Grid>
                     </a>
                 </Card>                    
-                </Grid>
-                <Grid item lg = {6}>
-                <Card className = 'event'>
-                    <a href = 'https://www.theatlantic.com/culture/archive/2020/07/your-anti-racism-books-are-means-not-end/614281/?fbclid=IwAR3IqyZVKadfUT5OC6O6L0hHaFfNYkiRYpnsQ-k6QOJktqjeLnJyOOpTa_Q'>
-                        <Grid container justify = "center"  style= {{ padding: 0}}>
-                            <Grid item lg = {4} sm = {12}>
-                                <img src= "blm.png" style= {{width:"125px", height: "75px", padding: 0}} alt = 'The False Promise of Anti-racism Books'/>
-                            </Grid>
-                            <Grid item lg = {8} sm = {12} className = 'articled'>
-                                <p>
-                                The False Promise of Anti-racism Books
-                                </p>
-                            </Grid>
-                        </Grid>
-                    </a>
-                </Card>  
-                </Grid>
-                <Grid lg = {6}>
-
                 </Grid>
                 </Grid>
             <Divider/>
